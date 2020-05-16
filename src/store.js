746402aac4dab761365a56665af590e5bd6dbbc5
lioartoil <a.chanakant@gmail.com>
2020-05-16 16:48:36 +0700

    Mastering Vuex: Lesson 3: State & Getters

--- src/store.js
+++ src/store.js
@@ -5,8 +5,36 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    user: { id: 'abc123' },
+    user: { id: 'abc123', name: 'Adam Jahr' },
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community',
+    ],
+    todos: [
+      { id: 1, text: '...', done: true },
+      { id: 2, text: '...', done: false },
+      { id: 3, text: '...', done: true },
+      { id: 4, text: '...', done: false },
+    ],
+    events: [
+      { id: 1, title: '...', organizer: '...' },
+      { id: 2, title: '...', organizer: '...' },
+      { id: 3, title: '...', organizer: '...' },
+      { id: 4, title: '...', organizer: '...' },
+    ],
   },
   mutations: {},
   actions: {},
+  getters: {
+    activeTodosCount: ({ todos }) => todos.filter(({ done }) => !done).length,
+    catLength: ({ categories: { length } }) => length,
+    doneTodos: ({ todos }) => todos.filter(({ done }) => done),
+    getEventById: ({ events }) => (id) =>
+      events.find((event) => event.id === id),
+  },
 })
